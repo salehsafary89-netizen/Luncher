1c78e55943487a937661d23d1a93d13d808f4135
salehsafary89-netizen <salehsafary89@gmail.com>
2026-08-01 04:05:27 +0330

    Create app/src/main/java/com/salehsafary/darkcalc/ui/theme/Theme.kt

---
+++ app/src/main/java/com/salehsafary/darkcalc/ui/theme/Theme.kt
@@ -0,0 +1,11 @@
+app
+└── src
+    └── main
+        └── java
+            └── com
+                └── salehsafary
+                    └── darkcalc
+                        ├── MainActivity.kt
+                        └── ui
+                            └── theme
+                                └── Theme.kt
