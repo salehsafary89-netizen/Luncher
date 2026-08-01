22aa9172eb1c400fe52272b966db5327159bc0df
salehsafary89-netizen <salehsafary89@gmail.com>
2026-08-01 03:57:45 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
         applicationId = "com.salehsafary.darkcalc"
         minSdk = 26
         targetSdk = 35
+
         versionCode = 1
         versionName = "0.1.0"
     }
@@ -20,9 +21,20 @@ android {
         compose = true
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+            excludes += "/META-INF/LGPL2.1"
+            excludes += "/META-INF/AL2.0"
         }
     }
 }
@@ -32,6 +44,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.10.1")
 
     implementation(platform("androidx.compose:compose-bom:2025.01.01"))
+
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
